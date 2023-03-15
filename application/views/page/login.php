8e254da7ae243b76623f9ce336f6a792b5f3af64
nohan24 <nohanrazafinjato@gmail.com>
2023-03-15 21:00:46 +0300

    login

--- application/views/page/login.php
+++ application/views/page/login.php
@@ -1,6 +1,14 @@
 <div class="container">
     <div class="box login_box">
         <h2>Log - in</h2>
+        <?php
+            if ($err == 3) { ?>
+        <div class="alert alert-danger">
+            <p>Le nom ou le mot de passe est invalide</p>
+        </div>
+        <?php
+            }
+        ?>
         <form action="<?php echo site_url('login'); ?>" method="post">
             <p><?php echo form_error('nom'); ?></p>
             <input type="text" class="input" placeholder="Entrer le nom de votre société" name="nom">
@@ -9,16 +17,8 @@
             <input type="submit" value="Se connecter" class="btn btn-primary">
         </form>
         <div class="links">
-            <p>Pas encore enregitré ? <a href="<?php site_url('login/inscription'); ?>" class="link">S'incrire</a></p>
+            <p>Pas encore enregitré ? <a href="<?php echo site_url('login/inscription'); ?>" class="link">S'incrire</a>
+            </p>
         </div>
     </div>
-    <?php
-        if (isset($err)) {
-            if ($err == 3) { ?>
-    <div class="alert alert-danger">
-        <p>Le nom ou le mot de passe est invalide</p>
-    </div>
-    <?php }
-        }
-    ?>
 </div>
