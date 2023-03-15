c010405fa7ab0764a9553f861ad5a7e8cb4f0be7
nohan24 <nohanrazafinjato@gmail.com>
2023-03-15 20:56:04 +0300

    login error

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -8,28 +8,26 @@ class Login extends CI_Controller {
         $data['err'] = $error;
         $data['title'] = 'Login';
         $this->load->view('template/header.php',$data);
-		$this->load->view('page/login'); // modifiena par vue hafa
+        if($this->input->post()){
+            $this->form_validation->set_rules('nom','nom','required',array('required' => 'Ce champ doit-être rempli'));
+            $this->form_validation->set_rules('pwd','mot de passe','required',array('required' => 'Ce champ doit-être rempli'));
+            if($this->form_validation->run() === FALSE){
+                $this->load->view('page/login');
+            }else{
+                $this->load->model('sign_model');
+                $res = $this->sign_model->checkSignIn($_POST['nom'],$_POST['pwd']);
+                if($res == 3){
+                    $data['err'] = 3;
+                    $this->load->view('page/login');
+                }
+            }
+        }else{
+            $this->load->view('page/login');
+        }
         $this->load->view('template/ending.php');
 	}
 
     public function inscription(){
         $data['title'] = 'Inscription';
-    }
-
-    public function log(){
-        if($this->input->post()){
-            $this->form_validation->set_rules('nom','nom','required',array('required' => 'Ce champ doit-être rempli'));
-            $this->form_validation->set_rules('pwd','mot de passe','required',array('required' => 'Ce champ doit-être rempli'));
-            if($this->form_validation->run() === FALSE){
-                $this->load->view('login');
-            }else{
-                $this->load->model('sign_model');
-                $res = $this->sign_model->checkSignIn($_POST['nom'],$_POST['pwd']);
-                if($res == 3){
-                    redirect(site_url('home/error/3'));
-                }
-            }
-            $data['title'] = "Ajout";
-        }
     }
 }
--- application/views/page/login.php
+++ application/views/page/login.php
@@ -1,22 +1,22 @@
 <div class="container">
     <div class="box login_box">
-        <h2>Log in</h2>
-        <form action="<?php site_url('login/log'); ?>" method="post">
-            <input type="text" class="input" placeholder="Entrer le nom de votre société" name="nom" required="">
-
-            <input type="password" class="input" name="pwd" required="" placeholder="Mot de passe">
-
+        <h2>Log - in</h2>
+        <form action="<?php echo site_url('login'); ?>" method="post">
+            <p><?php echo form_error('nom'); ?></p>
+            <input type="text" class="input" placeholder="Entrer le nom de votre société" name="nom">
+            <p><?php echo form_error('pwd'); ?></p>
+            <input type="password" class="input" name="pwd" placeholder="Mot de passe">
             <input type="submit" value="Se connecter" class="btn btn-primary">
         </form>
         <div class="links">
-            <p>Pas encore enregitré ? <a href="#" class="link">S'incrire</a></p>
+            <p>Pas encore enregitré ? <a href="<?php site_url('login/inscription'); ?>" class="link">S'incrire</a></p>
         </div>
     </div>
     <?php
         if (isset($err)) {
             if ($err == 3) { ?>
     <div class="alert alert-danger">
-        <p>Mot de passe invalide</p>
+        <p>Le nom ou le mot de passe est invalide</p>
     </div>
     <?php }
         }
