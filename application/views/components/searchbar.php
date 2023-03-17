4636281dc22460d0fd7e1f7ec48e3235be6b2e5c
hagajoel <joel.andriamasivelo@gmail.com>
2023-03-17 10:19:47 +0100

    homepage

---
+++ application/views/components/searchbar.php
@@ -0,0 +1,8 @@
+<div class="search-bar">
+			<form action="" method="post">
+				<div class="search">
+					<input type="text" class="input_search" name="search" placeholder="Rechercher">
+					<img src="<?php echo site_url('assets/img/icon_search.svg');?>" alt="">
+				</div>
+			</form>
+		</div>
--- application/views/page/home.php
+++ application/views/page/home.php
@@ -1,7 +1,8 @@
 	<?php
-		$this->load->view('template/sidebar.php');
+		$this->load->view('components/sidebar.php');
 	?>
 	<div class="main-page">
-		
+		<?php $this->load->view('components/searchbar.php');?>
+		<p>Aucun donnée</p>
 	</div>
 
