83bbb87a2eeb19ff53323e6ed596067c3a5fd01c
hagajoel <joel.andriamasivelo@gmail.com>
2023-03-17 17:01:26 +0100

    upload csv file pgc

--- application/views/page/pcg.php
+++ application/views/page/pcg.php
@@ -5,11 +5,14 @@
 		<div class="box">
 			<h3>Insérer un plan comptable de gestion</h3>
 			<form action="">
-					
-					<input type="text" class="input" name="numero" placeholder="ex: 35">
-					<input type="text" class="input" name="intitule" placeholder="Stocks de produits">
+				<input type="text" class="input" name="numero" placeholder="ex: 35">
+				<input type="text" class="input" name="intitule" placeholder="Stocks de produits">
 				<input type="submit" class="btn btn-primary" value="Insérer">
-			</form>
+ 			</form>
+			<div class="upload_container">
+				<label for="file"> <img src="<?php echo site_url('assets/img/file-arrow-down-solid.svg');?>" alt=""> Import un fichier CSV</label>
+				<input type="file" id="file" class="input_file">
+				<p class="file_name">pgc.csv</p>
+			</div>
 		</div>
 	</div>
-
