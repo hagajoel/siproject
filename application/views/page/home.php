dc241c6d8c93c75ab6652eabaacb21a3e7e4a1f0
hagajoel <joel.andriamasivelo@gmail.com>
2023-03-17 09:49:27 +0100

    sidebar

--- application/views/page/home.php
+++ application/views/page/home.php
@@ -1,31 +1,7 @@
-<nav class="sidebar">
-	<a href="#" class="logo">
-		<img src="<?php echo site_url('assets/img/logo.png');?>" alt="logo">
-	</a>
-	<div class="search-bar">
-		<form action="">
-			<div class="search">
-				<input type="text" class="search_input" name="search">
-				<img src="<?php echo site_url('assets/img/icon_search.svg');?>" alt="icon">
-			</div>
-		</form>
+	<?php
+		$this->load->view('template/sidebar.php');
+	?>
+	<div class="main-page">
+		
 	</div>
-	<ul class="navlink">
-		<li class="nav_item">
-			<img src="<?php echo site_url('assets/img/1.svg');?>" alt="icon">
-			<a href="#">PCG</a>
-		</li>
-		<li class="nav_item">
-			<img src="<?php echo site_url('assets/img/2.svg');?>" alt="icon">
-			<a href="#">Journal</a>
-		</li>
-		<li class="nav_item">
-			<img src="<?php echo site_url('assets/img/devise.svg');?>" alt="icon">
-			<a href="#">Devise</a>
-		</li>
-		<li class="nav_item">
-			<img src="<?php echo site_url('assets/img/6.svg');?>" alt="icon">
-			<a href="#">A propos</a>
-		</li>
-	</ul>
-</nav>
+
